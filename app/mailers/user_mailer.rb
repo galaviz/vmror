8da8d7cee1a331e6823768640bc1be90d192ef5b
Yaser Almasri <yalmasri@keepmoving.com.mx>
2014-12-02 17:40:08 -0600

    y9 user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,13 @@ class UserMailer < ActionMailer::Base
   default from: "energymonitoring@verdemonarca.com"
 
   def send_test_email()
-    mail(to: "mauriziocalo07@gmail.com", subject: 'Test')
+    mail(to: "yalmasri@keepmoving.com.mx", subject: 'Test')
   end
 
   def send_welcome_email(user)
     @user = user
     @propuesta= @user.crear_propuesta()
-    mail(to: "admin@verdemonarca.com", subject: 'Usuario se ha registrado a verdemonarca')
+    mail(to: "yalmasri@keepmoving.com.mx", subject: 'Usuario se ha registrado a verdemonarca')
   end
 
   def send_donation_confirmation_email(user, amount, creditos_vm, codigo_vm)
@@ -16,7 +16,7 @@ class UserMailer < ActionMailer::Base
     @amount = amount
     @creditos_vm = creditos_vm
     @codigo_vm = codigo_vm
-    mail(to: "admin@verdemonarca.com", subject: 'Contribución Pay-It-Forward')
+    mail(to: "yalmasri@keepmoving.com.mx", subject: 'Contribución Pay-It-Forward')
   end
 
   def send_purchase_confirmation_email_to_user(user, cart)
@@ -34,7 +34,7 @@ class UserMailer < ActionMailer::Base
     cart.keys.each do |key|
       @items_and_counts_arr << {'item' => Item.find_by_id(key), 'quantity' => cart[key]}
     end
-    mail(to: "admin@verdemonarca.com" , subject: 'Compra en la Tienda VM')
+    mail(to: "yalmasri@keepmoving.com.mx" , subject: 'Compra en la Tienda VM')
   end
 
 end
