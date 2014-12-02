9f507c262784f6e2815d25041a93af5002f3170a
Yaser Almasri <yalmasri@keepmoving.com.mx>
2014-12-02 16:27:40 -0600

    y7 remove domain all from session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Verdemonarcaonrails::Application.config.session_store :cookie_store, key: '_verdemonarcaonrails_session', :domain => :all
+Verdemonarcaonrails::Application.config.session_store :cookie_store, key: '_verdemonarcaonrails_session'
