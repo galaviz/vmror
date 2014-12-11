ca5b2bd0f9ec42086c2e50a8c3a7296cb18fe3a1
Lauro Antonio Galaviz Castro <galavizcastro@gmail.com>
2014-12-11 15:48:04 -0600

    nada
    nada

---
+++ public/assets/ajax-b140245fda818d75aebbeb20f57b5993.js
@@ -0,0 +1,25 @@
+function Ajax(thisData, thisLocation,thisSuccess, thisError){
+    $.ajax({
+          type: "POST",
+          url: thisLocation,
+          data: thisData,
+          dataType: "json",
+          beforeSend: function(){
+	    
+          },
+          success: function(pResponse) {
+			  if (pResponse.success === 1) {
+				thisSuccess(pResponse);                                                            
+			  } else {
+				thisError(pResponse);
+			  }
+		  },
+          error: function() {
+	    
+          },
+          complete: function() {
+	    
+          }
+    });
+}
+;
