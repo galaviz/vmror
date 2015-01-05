997d31cc3799b25ea5d0a5687dcc0d36e62e3f2a
Lauro Antonio Galaviz Castro <galavizcastro@gmail.com>
2015-01-05 14:04:37 -0600

    db migrate
    db migrate

--- db/migrate/20150102203842_create_product_sizes.rb
+++ db/migrate/20150102203842_create_product_sizes.rb
@@ -1,8 +1,8 @@
 class CreateProductSizes < ActiveRecord::Migration
   def change
     create_table :product_sizes do |t|
+    	t.string :size_description
     	t.float :price
-    	t.integer :size_id
     	t.integer :item_id
       t.timestamps
     end
